ac08f019422a854fb294dd7a542b564e7c8ec3a0
Ryuunosuke Yoshikawa <reikoyoshikawa@outlook.com>
2017-12-31 23:59:27 +0700

    HAPPY NEW YEAR SUKA BLYAT ME ROLLS CAKES LMAO

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -36,7 +36,7 @@
 
 /*
  * Targeted preemption latency for CPU-bound tasks:
- * (default: 1ms * (1 + ilog(ncpus)), units: nanoseconds)
+ * (default: 4ms * (1 + ilog(ncpus)), units: nanoseconds)
  *
  * NOTE: this latency value is not the same as the concept of
  * 'timeslice length' - timeslices in CFS are of variable length
@@ -46,8 +46,8 @@
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
  */
-unsigned int sysctl_sched_latency = 1000000ULL;
-unsigned int normalized_sysctl_sched_latency = 1000000ULL;
+unsigned int sysctl_sched_latency = 4000000ULL;
+unsigned int normalized_sysctl_sched_latency = 4000000ULL;
 
 /*
  * The initial- and re-scaling of tunables is configurable
@@ -63,15 +63,15 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling
 
 /*
  * Minimal preemption granularity for CPU-bound tasks:
- * (default: 0.10 msec * (1 + ilog(ncpus)), units: nanoseconds)
+ * (default: 0.80 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity = 100000ULL;
-unsigned int normalized_sysctl_sched_min_granularity = 100000ULL;
+unsigned int sysctl_sched_min_granularity = 800000ULL;
+unsigned int normalized_sysctl_sched_min_granularity = 800000ULL;
 
 /*
  * is kept at sysctl_sched_latency / sysctl_sched_min_granularity
  */
-static unsigned int sched_nr_latency = 1;
+static unsigned int sched_nr_latency = 8;
 
 /*
  * After fork, child runs first. If set to 0 (default) then
