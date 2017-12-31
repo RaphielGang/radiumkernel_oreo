32eed65af1f0bf23c705915fcb88447505cf9be0
Ryuunosuke Yoshikawa <reikoyoshikawa@outlook.com>
2017-12-31 20:06:00 +0700

    sched-fair : updated timeticks, for the sake of nee-sama new year meme again

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -36,7 +36,7 @@
 
 /*
  * Targeted preemption latency for CPU-bound tasks:
- * (default: 5ms * (1 + ilog(ncpus)), units: nanoseconds)
+ * (default: 4ms * (1 + ilog(ncpus)), units: nanoseconds)
  *
  * NOTE: this latency value is not the same as the concept of
  * 'timeslice length' - timeslices in CFS are of variable length
@@ -46,12 +46,12 @@
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
  */
-unsigned int sysctl_sched_latency = 5000000ULL;
-unsigned int normalized_sysctl_sched_latency = 5000000ULL;
+unsigned int sysctl_sched_latency = 4000000ULL;
+unsigned int normalized_sysctl_sched_latency = 4000000ULL;
 
 /*
  * The initial- and re-scaling of tunables is configurable
- * (default SCHED_TUNABLESCALING_LOG = *(1+ilog(ncpus))
+ * (default SCHED_TUNABLESCALING_LINEAR - scaled linear, *ncpus)
  *
  * Options are:
  * SCHED_TUNABLESCALING_NONE - unscaled, always *1
@@ -59,14 +59,14 @@ unsigned int normalized_sysctl_sched_latency = 5000000ULL;
  * SCHED_TUNABLESCALING_LINEAR - scaled linear, *ncpus
  */
 enum sched_tunable_scaling sysctl_sched_tunable_scaling
-	= SCHED_TUNABLESCALING_LOG;
+	= SCHED_TUNABLESCALING_LINEAR;
 
 /*
  * Minimal preemption granularity for CPU-bound tasks:
- * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
+ * (default: 0.80 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity = 750000ULL;
-unsigned int normalized_sysctl_sched_min_granularity = 750000ULL;
+unsigned int sysctl_sched_min_granularity = 800000ULL;
+unsigned int normalized_sysctl_sched_min_granularity = 800000ULL;
 
 /*
  * is kept at sysctl_sched_latency / sysctl_sched_min_granularity
