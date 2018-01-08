fc4942bc6fa414cdc9a2b31cd2aa4b9cec36596f
Ryuunosuke Yoshikawa <reikoyoshikawa@outlook.com>
2018-01-09 03:55:17 +0700

    ipa everytime : my trace derps

--- drivers/platform/msm/ipa/ipa_v2/ipa.c
+++ drivers/platform/msm/ipa/ipa_v2/ipa.c
@@ -38,7 +38,7 @@
 #include "../ipa_rm_i.h"
 
 #define CREATE_TRACE_POINTS
-#include <trace/ipa_trace.h>
+#include <trace/ipa_trace_v2.h>
 
 #define IPA_SUMMING_THRESHOLD (0x10)
 #define IPA_PIPE_MEM_START_OFST (0x0)
